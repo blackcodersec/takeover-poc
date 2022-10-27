a5664aa55e5f4aba6584d308456abf436265fb6b
blackcodersec <72689232+blackcodersec@users.noreply.github.com>
2022-10-27 19:04:39 +0600

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -4,7 +4,6 @@
 #$HOST should be in URL format -- http://IP:Port (https://111.222.333.44:8000)
 
 HOST=$1
-echo -e "\e[34m[*] Scanning $HOST\e[0m"
 curl $HOST/wls-wsat/CoordinatorPortType -k -s -m1 | grep "<h1>Web Services"
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
